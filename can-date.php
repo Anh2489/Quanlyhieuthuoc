fb595c3a716733c57dcb4a8e705b99186d38c34e
Anh2489 <2221050401@student.humg.edu.vn>
2026-01-07 12:10:25 +0700

    Add product expiration warning to can-date.php
    Added functionality to display products nearing expiration within 30 days.

--- can-date.php
+++ can-date.php
@@ -1,48 +1,51 @@
 <?php
 include("connect.php");
-mysqli_set_charset($conn, "utf8");
 
-// Lấy danh sách thuốc sắp hết hạn (<= 30 ngày tới) kèm tồn kho
-$sql_expire = "SELECT t.ma_thuoc, t.ten_thuoc, t.han_su_dung, k.ton_kho
-    FROM thuoc t
-    LEFT JOIN kho k ON t.ma_kho = k.ma_kho
-    WHERE t.han_su_dung <= DATE_ADD(CURDATE(), INTERVAL 30 DAY)
-    ORDER BY t.han_su_dung ASC";
+// Lấy danh sách sản phẩm (sp) sắp hết hạn (<= 30 ngày tới) kèm tồn kho
+$sql_expire = "
+    SELECT 
+        s.ma_sp, 
+        s.ten_sp, 
+        s.han_su_dung, 
+        k.ton_kho,
+        DATEDIFF(s.han_su_dung, CURDATE()) as so_ngay
+    FROM sp s
+    LEFT JOIN kho k ON s.ma_kho = k.ma_kho
+    WHERE s.han_su_dung <= DATE_ADD(CURDATE(), INTERVAL 30 DAY)
+    ORDER BY s.han_su_dung ASC
+";
     
 $result_expire = $conn->query($sql_expire);
-$sap_het_han = $result_expire ? $result_expire->num_rows : 0;
-
-
 ?>
 
 <div class="table-box">
-    <h2 style="text-align:center">Danh sách thuốc sắp hết hạn (dưới 30 ngày)</h2>
-    <table border="0" width="100%" cellspacing="0" cellpadding="8">
+    <h2 style="text-align:center">Cảnh báo thuốc sắp hết hạn (30 ngày)</h2>
+    <table width="100%" cellspacing="0" cellpadding="8">
         <thead>
             <tr>
                 <th>Mã thuốc</th>
                 <th>Tên thuốc</th>
                 <th>Hạn sử dụng</th>
-                <th>Số lượng tồn</th>
+                <th>Còn lại</th>
+                <th>Tồn</th>
             </tr>
         </thead>
         <tbody>
-            <?php if ($sap_het_han > 0): ?>
-                <?php while ($thuoc = $result_expire->fetch_assoc()): ?>
+            <?php if ($result_expire && $result_expire->num_rows > 0): ?>
+                <?php while ($r = $result_expire->fetch_assoc()): ?>
                     <tr>
-                        <td><?= $thuoc['ma_thuoc'] ?></td>
-                        <td><?= $thuoc['ten_thuoc'] ?></td>
-                        <td style="color:red;">
-                            <?= date("d-m-Y", strtotime($thuoc['han_su_dung'])) ?>
+                        <td><?= $r['ma_sp'] ?></td>
+                        <td><?= $r['ten_sp'] ?></td>
+                        <td style="color:red; font-weight:bold;">
+                            <?= date("d/m/Y", strtotime($r['han_su_dung'])) ?>
                         </td>
-                        <td><?= $thuoc['ton_kho'] ?? 0 ?></td>
+                        <td><?= $r['so_ngay'] ?> ngày</td>
+                        <td><b><?= $r['ton_kho'] ?? 0 ?></b></td>
                     </tr>
                 <?php endwhile; ?>
             <?php else: ?>
                 <tr>
-                    <td colspan="4" style="text-align:center;">
-                        Không có thuốc nào sắp hết hạn
-                    </td>
+                    <td colspan="5" style="text-align:center;">Không có thuốc nào sắp hết hạn</td>
                 </tr>
             <?php endif; ?>
         </tbody>
